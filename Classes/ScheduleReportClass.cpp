e32ea94f4019cd910dea45f6772ce60bc7c2a66c
DJ <douglas.gillory@gmail.com>
2021-04-29 15:15:33 -0400

    Updated Scheduled Report ready for testing

--- Classes/ScheduleReportClass.cpp
+++ Classes/ScheduleReportClass.cpp
@@ -1,8 +1,15 @@
 /*This class "prints" displays the Schedule Reports to be emailed or printed to
 the staff firm requesting the report.*/
 
+#include <string>
+#include <iostream>
+#include <ScheduleClass.cpp>
+using namespace std;
+
 	class ScheduleReport {
-		void printReport() {
+
+	string schedule.getprintReport(string printReport) {
+		
 			return printReport();
 		}
 	};
