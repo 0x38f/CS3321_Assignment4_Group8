dc4283cfdc220cec0db89ac44ac2a6e03633df7a
DJ <douglas.gillory@gmail.com>
2021-04-29 15:10:27 -0400

    Update Classes ready for testing

--- Classes/CommunicationClass.cpp
+++ Classes/CommunicationClass.cpp
@@ -1,19 +1,68 @@
 /*The Communication class derives the chat transcript from the Request Class and emails the record to the associated firm.*/
+#include <iostrem>
+#include <string>
+#include <iomanip>
+#include <RequestClass.cpp>
+#include <stdafx.h>
+#include <array>
+using namespace std;
+using namespace System;
+using namespace EASendMail;
 
 class Communication {
 
-	Request request;
-	
-	
-	void email() {
-		if (request.getChatTranscript() != empty())
-			send email(request.getChatTranscript());
-	}
 
+    void email(array<System::String^>^ args) {
+
+        try
+        {
+            SmtpMail^ oMail = gcnew SmtpMail("TryIt");
+
+            // Your Gmail email address
+            oMail->From = "deponmore@gmail.com";
+            // Set recipient email address, please change it to yours
+            oMail->To = "support@deponmore.com";
+
+            // Set email subject
+            oMail->Subject = "test email from Managed C++ with Gmail account";
+            // Set email body
+            oMail->TextBody = "this is a test email sent from Managed C++ project with gmail";
+
+            // Gmail SMTP server address
+            SmtpServer^ oServer = gcnew SmtpServer("smtp.gmail.com");
+            // Gmail user authentication
+            oServer->User = "gmailid@gmail.com";
+            oServer->Password = "********";
+
+            // Use 465 port
+            oServer->Port = 465;
+
+            // detect SSL/TLS automatically
+            oServer->ConnectType = SmtpConnectType::ConnectSSLAuto;
+
+            Console::WriteLine("start to send email with SSL connection...");
+
+            SmtpClient^ oSmtp = gcnew SmtpClient();
+            oSmtp->SendMail(oServer, oMail);
+
+            Console::WriteLine("email was sent successfully!");
+        }
+        catch (Exception^ ep)
+        {
+            Console::WriteLine("failed to send email with the following error:");
+            Console::WriteLine(ep->Message);
+        }
+
+    }
+
+    //Displays chat transcript for viewer
 	void displayMessage() {
-		int chat = request.getChatTranscript.length();
-		for (int i = 0; i < chat; i++) {
-			if (chat != 0) {
+        Request request;
+
+		string chat = request.getChatTranscript();
+
+        //traverse and display the chat transcript
+		for (int i = 0; i != chat.end(); i++) {
 				cout << request.getChatTranscript() << endl;
 			}
 		}
--- Classes/DepositionReportClass.cpp
+++ Classes/DepositionReportClass.cpp
@@ -1,8 +1,14 @@
 /*This class "prints"/displays the Depositions to be emailed or printed to
  the inquiring staff/firm.*/
+ #include <string>
+ #include <iostream>
+ #include <DepositionClass.cpp>
+ using namespace std;
+
 
 	class DepositionReport {
-		void printReport() {
-			return printReport();
+		//Getter to access report from Deposition Class
+		string deposition.getprintReport(string printReport) {
+			return printReport;
 		}
 	};
