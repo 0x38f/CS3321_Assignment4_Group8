b5fef5914fc2d0008b0080eec933d02dbaf8c086
Phillip Sanders <phj.sanders@gmail.com>
2021-05-01 14:07:52 -0500

    Testing Registration class

--- Testing/UI-Register_Test.cpp
+++ Testing/UI-Register_Test.cpp
@@ -1,11 +1,17 @@
 #include "Classes/UserInterfaceClass.cpp"
+#include "Classes/RegisterClass.cpp"
 
 int main () {
 
     UserInterface UITest;
+    Register RegisterTest
 
     UITest.loginUI();
 
     UITest.siteMenu();
+
+    int testID = RegisterTest.getUserID();
+
+    cout << testID;
 
 }
