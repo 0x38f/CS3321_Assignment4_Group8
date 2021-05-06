22d82a61e1bb78585ebd1eae666b2a64d4c4623d
colem08 <82627698+colem08@users.noreply.github.com>
2021-05-06 14:49:32 -0500

    Update RequestClass.cpp

--- Classes/RequestClass.cpp
+++ Classes/RequestClass.cpp
@@ -27,23 +27,27 @@ private:
 
 		//Routes to Correct Staff member based on Topic
 		void routeStaff() {
-			string helpTopic, staff1, staff2, staff3, staff4;
-			for (int i = 0; i < helpTopic.length(); i++) {
+			string helpTopic, staff1, staff2, staff3, staff4, rstaff;
+			for (int i = 0; i < helpTopic.lenght(); i++) {
 				if (helpTopic == i) {
-					switch (i) {
-					case1: send(staff1);
+					switch (helpTopic) {
+					case 1: send(staff1);
+					       rstaff= staff1; 
 						break;
 					case 2: send(staff2);
+						rstaff=staff2;
 						break;
 					case 3: send(staff3);
+						rstaff=staff3;
 						break;
 					case 4: send(staff4);
+						rstaff=staff4;
 						break;
 
 					}
 				}
 			}
-
+                 return rstaff; // returns the staff number being assigned
 		}
 
 
