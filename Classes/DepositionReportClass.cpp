3bbc579ede13cc4c97c38c81af0ea4568e71df62
DJ <douglas.gillory@gmail.com>
2021-05-10 11:38:25 -0500

    Updated and ready for testing

--- Classes/DepositionReportClass.cpp
+++ Classes/DepositionReportClass.cpp
@@ -1,17 +1,22 @@
 /*This class "prints"/displays the Depositions to be emailed or printed to
  the inquiring staff/firm.*/
 
- #include <string>
- #include <iostream>
-
- #include "DepositionClass.cpp"
- using namespace std;
-
+#include <string>
+#include <iostream>
+#include "DepositionClass.cpp"
+using namespace std;
 
 
-	class DepositionReport {
-		//Getter to access report from Deposition Class
-		string deposition.getprintReport(string printReport) {
-			return printReport;
-		}
-	};
+class DepositionReport {
+
+	//Getter to access report from Deposition Class
+	void printReport() {
+		Deposition depo;
+
+		cout << depo.getUpdate();
+
+	}
+
+
+
+};
